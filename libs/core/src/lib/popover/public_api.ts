a86974b0ce596ee257b285142240be9fa10d14e1
Jedrzej <jedrzej.markowski@sap.com>
2019-11-21 17:32:41 +0100

    Update usage of side nav, add important files of popover to public_api.ts

--- libs/core/src/lib/popover/public_api.ts
+++ libs/core/src/lib/popover/public_api.ts
@@ -1,2 +1,6 @@
 export * from './popover.module';
 export * from './popover-directive/popover.directive';
+export * from './popover.component';
+export * from './popover-control/popover-control.component';
+export * from './popover-body/popover-body.component';
+export * from './popover-directive/popover-container';
